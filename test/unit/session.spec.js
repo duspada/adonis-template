a912a031705e8c67718c6c698784cf868bb98fef
Du Spada <du.spada@gmail.com>
2019-09-29 21:32:45 -0300

    Create User

---
+++ test/unit/session.spec.js
@@ -0,0 +1,26 @@
+const { test, trait } = use('Test/Suite')('Session')
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use('Factory')
+
+trait('Test/ApiClient')
+
+test('it should be able to login', async ({ client, assert }) => {
+  const userData = {
+    name: 'teste',
+    email: 'a@b.c',
+    password: '123456'
+  }
+
+  await Factory
+    .model('App/Models/User')
+    .create(userData)
+
+  const response = await client
+    .post('/sessions')
+    .send(userData)
+    .end()
+
+  response.assertStatus(200)
+  assert.exists(response.body.token)
+})
